55ee199387eb43f61167bb71996f9987e5ba573c
Jhon9191 <joaoantonio15161516@gmail.com>
2021-02-26 10:50:34 -0300

    Starting the countdown

--- src/components/Countdown/Countdown.tsx
+++ src/components/Countdown/Countdown.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { AuthContext } from '../../contexts/auth';
 import { View, Text, TouchableOpacity } from 'react-native';
 
@@ -6,29 +6,64 @@ import styles from './styles';
 
 const Countdown = () => {
     const { } = useContext(AuthContext);
+    const [time, setTime] = useState(25*60);
+    const [isActive, setIsActive] = useState(false);
+
+    const minutes = Math.floor(time/60);
+    const seconds = time%60;
+
+    const [minutesLeft, minutesRight] = String(minutes).padStart(2, '0').split('');
+    const [secondsLeft, secondsRight] = String(seconds).padStart(2, '0').split('');
+
+    const startCountdown = () =>{
+        setIsActive(true);
+    }
+
+    useEffect(()=>{
+        if(isActive && time > 0){
+            setTimeout(()=>{
+                setTime(time - 1);
+            }, 1000)
+        }
+    },[isActive, time])
+
     return (
         <View style={styles.container}>
-            <View style={{ flexDirection: 'row', display: 'flex', alignItems: 'center', padding:30 }}>
+            <View style={{ flexDirection: 'row', display: 'flex', alignItems: 'center', padding: 30 }}>
+
                 <View style={styles.card}>
-                    <Text>2</Text>
+                    <Text style={styles.textCountdown}>
+                        {minutesLeft}
+                    </Text>
                 </View>
+
                 <View style={styles.card}>
-                    <Text>5</Text>
+                    <Text style={styles.textCountdown}>
+                        {minutesRight}
+                    </Text>
                 </View>
 
                 <Text style={{ fontSize: 40 }}>:</Text>
 
                 <View style={styles.card}>
-                    <Text>0</Text>
+                    <Text style={styles.textCountdown}>
+                        {secondsLeft}
+                    </Text>
                 </View>
+
                 <View style={styles.card}>
-                    <Text>0</Text>
+                    <Text style={styles.textCountdown}>
+                        {secondsRight}
+                    </Text>
                 </View>
+
             </View>
 
-            <TouchableOpacity style={styles.comecarCiclo}>
+            <TouchableOpacity style={styles.comecarCiclo}
+            onPress={startCountdown}
+            >
                 <View>
-                <Text>Começar ciclo</Text>
+                    <Text>Começar ciclo</Text>
                 </View>
             </TouchableOpacity>
 
--- src/components/Countdown/styles.tsx
+++ src/components/Countdown/styles.tsx
@@ -9,19 +9,28 @@ const styles = StyleSheet.create({
         width: '100%'
 
     },card:{
-        width: '25%',
+        width: '24%',
         height: 140,
         backgroundColor: "#f5f5f5",
         elevation:2,
         margin: 6,
         borderBottomColor: '#67ff6f',
-        borderBottomWidth: 4
+        borderBottomWidth: 4,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },textCountdown:{
+        fontSize: 50,
+        fontWeight: 'bold',
+        color: '#888888'
     }
 
     ,comecarCiclo:{
         width: '95%',
         height: 50,
-        backgroundColor: '#3527f0'
+        backgroundColor: '#5148d1',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 4
 
     }
 
